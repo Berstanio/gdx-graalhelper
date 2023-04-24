5e5263d26724f34e557cf9c5f72b98848a25f17f
Berstanio <berstaniodev@gmail.com>
2023-04-24 13:21:27 +0200

    feat: Remove need for jdk 11, but rely on internal API

--- src/main/java/com/anyicomplex/gdx/svm/AnnotationFeature.java
+++ src/main/java/com/anyicomplex/gdx/svm/AnnotationFeature.java
@@ -1,44 +1,36 @@
 package com.anyicomplex.gdx.svm;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
+import com.oracle.svm.hosted.FeatureImpl.FeatureAccessImpl;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeSerialization;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Vector;
+import java.util.stream.Collectors;
 
 @AutomaticFeature
 public class AnnotationFeature implements Feature {
 
 	@Override
 	public void beforeAnalysis (BeforeAnalysisAccess access) {
-		try {
-			Field f = ClassLoader.class.getDeclaredField("classes");
-			f.setAccessible(true);
-			ClassLoader classLoader = com.badlogic.gdx.files.FileHandle.class.getClassLoader();
-			Vector<Class<?>> vectorClasses = (Vector<Class<?>>)f.get(classLoader);
-			Class<?>[] classes = vectorClasses.toArray(new Class[0]);
-			ArrayList<String> collectedForReflection = new ArrayList<>();
-			ArrayList<String> collectedForSerialization = new ArrayList<>();
-			Arrays.stream(classes).forEach(aClass -> {
-				if (aClass.isAnnotationPresent(CollectForReflection.class)) {
-					FeatureUtils.registerForGdxInstantiation(aClass);
-					collectedForReflection.add(aClass.getName());
-				}
-				if (aClass.isAnnotationPresent(CollectForSerialization.class)) {
-					RuntimeSerialization.register(aClass);
-					collectedForSerialization.add(aClass.getName());
-				}
-			});
-			System.out.print("The following classes are added because of the CollectForReflection annotation: ");
-			System.out.println(String.join(", ", collectedForReflection));
-			System.out.print("The following classes are added because of the CollectForSerialization annotation: ");
-			System.out.println(String.join(", ", collectedForSerialization));
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			System.err.println("You are probably using Java 17 GraalVM. The annotation are not currently supported for that.");
-		}
+		FeatureAccessImpl impl = (FeatureAccessImpl) access;
+		impl.findAnnotatedClasses(CollectForReflection.class).forEach(FeatureUtils::registerForGdxJSONSerialization);
+		impl.findAnnotatedClasses(CollectForSerialization.class).forEach(RuntimeSerialization::register);
+
+		List<Class<?>> collectedForReflection = impl.findAnnotatedClasses(CollectForReflection.class);
+		collectedForReflection.forEach(FeatureUtils::registerForGdxJSONSerialization);
+
+		List<Class<?>> collectedForSerialization = impl.findAnnotatedClasses(CollectForSerialization.class);
+		collectedForSerialization.forEach(RuntimeSerialization::register);
+
+		System.out.print("The following classes are added because of the CollectForReflection annotation: ");
+		System.out.println(collectedForReflection.stream().map(Class::getName).collect(Collectors.joining(", ")));
+		System.out.print("The following classes are added because of the CollectForSerialization annotation: ");
+		System.out.println(collectedForSerialization.stream().map(Class::getName).collect(Collectors.joining(", ")));
 	}
 
 }
