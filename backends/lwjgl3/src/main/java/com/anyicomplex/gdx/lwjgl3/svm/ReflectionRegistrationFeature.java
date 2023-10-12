e00ba37de42296b4fe79e28c030644621b2d78c8
Berstanio <berstaniodev@gmail.com>
2023-10-12 14:49:04 +0200

    feat: Adjust for lwjgl 3.3.3

--- backends/lwjgl3/src/main/java/com/anyicomplex/gdx/lwjgl3/svm/ReflectionRegistrationFeature.java
+++ backends/lwjgl3/src/main/java/com/anyicomplex/gdx/lwjgl3/svm/ReflectionRegistrationFeature.java
@@ -17,8 +17,11 @@
 
 package com.anyicomplex.gdx.lwjgl3.svm;
 
+import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
+import org.graalvm.nativeimage.impl.ConfigurationCondition;
+import org.graalvm.nativeimage.impl.RuntimeResourceSupport;
 
 public class ReflectionRegistrationFeature implements Feature {
 
@@ -32,6 +35,16 @@ public class ReflectionRegistrationFeature implements Feature {
 
         try {
 
+            if (isWindows) {
+                ImageSingletons.lookup(RuntimeResourceSupport.class).addResources(ConfigurationCondition.alwaysTrue(), ".+\\.dll");
+            } else if (isLinux) {
+                ImageSingletons.lookup(RuntimeResourceSupport.class).addResources(ConfigurationCondition.alwaysTrue(), ".+\\.so");
+            } else if (isMac) {
+                ImageSingletons.lookup(RuntimeResourceSupport.class).addResources(ConfigurationCondition.alwaysTrue(), ".+\\.dylib");
+            } else {
+                throw new IllegalStateException("Unable to detect OS. Please report this.");
+            }
+
             // LWJGL3 backend
             // Can be fine-tuned to public Sound (OpenALLwjgl3Audio audio, FileHandle file)
             access.registerSubtypeReachabilityHandler((duringAnalysisAccess, aClass) -> RuntimeReflection.register(aClass.getDeclaredConstructors()),
@@ -40,28 +53,6 @@ public class ReflectionRegistrationFeature implements Feature {
             // Can be fine-tuned to public Music (OpenALLwjgl3Audio audio, FileHandle file)
             access.registerSubtypeReachabilityHandler((duringAnalysisAccess, aClass) -> RuntimeReflection.register(aClass.getDeclaredConstructors()),
                     com.badlogic.gdx.backends.lwjgl3.audio.OpenALMusic.class);
-
-            RuntimeReflection.register(sun.misc.Unsafe.class.getDeclaredField("theUnsafe"));
-            RuntimeReflection.register(java.nio.Buffer.class.getDeclaredFields());
-
-            // Could be maybe a bit fine-tuned, but should be fine
-            RuntimeReflection.register(access.findClassByName("java.nio.DirectByteBuffer").getDeclaredFields());
-            RuntimeReflection.register(access.findClassByName("java.nio.DirectShortBufferU").getDeclaredFields());
-            RuntimeReflection.register(access.findClassByName("java.nio.DirectCharBufferU").getDeclaredFields());
-            RuntimeReflection.register(access.findClassByName("java.nio.DirectIntBufferU").getDeclaredFields());
-            RuntimeReflection.register(access.findClassByName("java.nio.DirectLongBufferU").getDeclaredFields());
-            RuntimeReflection.register(access.findClassByName("java.nio.DirectFloatBufferU").getDeclaredFields());
-            RuntimeReflection.register(access.findClassByName("java.nio.DirectDoubleBufferU").getDeclaredFields());
-
-            RuntimeReflection.register(org.lwjgl.PointerBuffer.class.getDeclaredConstructors());
-            access.registerSubtypeReachabilityHandler((duringAnalysisAccess, aClass) ->
-                            // Would only need to be one constructor I think, but w/e
-                            RuntimeReflection.register(aClass.getDeclaredConstructors()),
-                    org.lwjgl.system.Struct.class);
-            access.registerSubtypeReachabilityHandler((duringAnalysisAccess, aClass) ->
-                            // Would only need to be one constructor I think, but w/e
-                            RuntimeReflection.register(aClass.getDeclaredConstructors()),
-                    org.lwjgl.system.StructBuffer.class);
 
         } catch (Throwable e) {
             throw new RuntimeException(e);
